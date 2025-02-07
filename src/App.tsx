6af02874f7c046e1e4694cb2e2474ace6ea4205e
AndrewwBC <andrew-hot2011@hotmail.com>
2025-02-07 20:15:17 -0300

    (Website) change name App to Website - add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Username from "./pages/User";
+
 function App() {
-  return <div>Hello, World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/user" element={<Username />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
