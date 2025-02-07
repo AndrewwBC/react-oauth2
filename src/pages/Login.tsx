a4cae68c61f97b0afbe97afeffa66d226fefb5ec
AndrewwBC <andrew-hot2011@hotmail.com>
2025-02-07 20:07:45 -0300

    add Login component

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <span>
+          Continue with GitHub{" "}
+          <a href="http://localhost:8080/oauth2/authorization/github">Link</a>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
