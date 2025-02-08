4958517c4cb330c33d5d9673f5bcda45013f4e01
AndrewwBC <andrew-hot2011@hotmail.com>
2025-02-07 22:40:56 -0300

    remove chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
+import { Provider } from "@/components/ui/provider";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Website from "./Website.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Website />
+    <Provider>
+      <Website />
+    </Provider>
   </StrictMode>
 );
